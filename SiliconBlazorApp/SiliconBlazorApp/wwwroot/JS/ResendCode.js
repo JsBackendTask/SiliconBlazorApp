30cec4e81be6e4b80b1f1493b0bdc38f6af07f48
Bnarington <36935528+Bnarington@users.noreply.github.com>
2024-05-23 16:53:55 +0200

    Done with logout and login functions.

---
+++ SiliconBlazorApp/SiliconBlazorApp/wwwroot/JS/ResendCode.js
@@ -0,0 +1,21 @@
+﻿function resendCode(email) {
+    fetch('https://verificationprovider-silicon-bnar.azurewebsites.net/api/Verify?code=GYUemekurKbemZgcZjQjK_WZHFRHCBUF6FHqAeYVq5BBAzFuDzsozw%3D%3D', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ Email: email })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error resending code');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Code resent');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
